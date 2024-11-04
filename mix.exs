c8f096ec9fb1e627c11ee60dc4f33b54497239f7
Sergi Martinez <sergimartinezrodriguez@gmail.com>
2024-11-04 05:29:36 -0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -30,34 +30,27 @@ defmodule BasedLanding.MixProject do
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
-  defp deps do
-    [
-      {:phoenix, "~> 1.7.14"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
-      {:heroicons,
-       github: "tailwindlabs/heroicons",
-       tag: "v2.1.1",
-       sparse: "optimized",
-       app: false,
-       compile: false,
-       depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
-    ]
-  end
+defp deps do
+  [
+    {:phoenix, "~> 1.7.14"},
+    {:phoenix_html, "~> 4.1"},
+    {:phoenix_live_reload, "~> 1.2", only: :dev},
+    {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+    {:floki, ">= 0.30.0", only: :test},
+    {:phoenix_live_dashboard, "~> 0.8.3"},
+    {:esbuild, "~> 0.8"},
+    {:tailwind, "~> 0.2"},
+    {:heroicons, github: "tailwindlabs/heroicons", tag: "v2.1.1", sparse: "optimized", app: false, compile: false, depth: 1},
+    {:swoosh, "~> 1.5"},
+    {:finch, "~> 0.13"},
+    {:telemetry_metrics, "~> 1.0"},
+    {:telemetry_poller, "~> 1.0"},
+    {:gettext, "~> 0.20"},
+    {:jason, "~> 1.2"},
+    {:dns_cluster, "~> 0.1.1"},
+    {:bandit, "~> 1.5"}
+  ]
+end
 
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to install project dependencies and perform other setup tasks, run:
